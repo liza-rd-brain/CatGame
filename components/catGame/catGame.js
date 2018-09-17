7a4e7a3a9a7ad7eef67c6d8341d86e46023ed025
MinakovaLisa <Giti528@ya.ru>
2018-09-17 15:10:12 +0300

    Препятствие может появляться на одном из трех уровней. Коллизия обрабатывается на каждом из них: теряется значок жизни. Кончились жизни => препятствие больше не ездит.

--- components/catGame/catGame.js
+++ components/catGame/catGame.js
@@ -4,9 +4,11 @@ function CatGame(container) {
     this.layersEl = null
     this.barrierEl = null
     this.healthLevelEl = null
-    //начальный уровень  0 - точка траснформации
-    this.checkCollisionTimerID = null
+    //начальный уровень  0 - точка траснформации   
+    this.timerCollision = null
+
     this.curentLevel = 0
+    // количество жизней
     this.levelCount = 3
     this.topCoord = "183px"
     this.middleCoord = "283px"
@@ -17,10 +19,12 @@ function CatGame(container) {
     this.smash = this.smash.bind(this)
     this.deleteHealthPoint = this.deleteHealthPoint.bind(this)
     this.goGame = this.goGame.bind(this)
+    this.stopCheckCollison = this.stopCheckCollison.bind(this)
     this.barrierMove = this.barrierMove.bind(this)
     this.barrierDelete = this.barrierDelete.bind(this)
+    this.barrierSelectLevel = this.barrierSelectLevel.bind(this)
 
-    this.checkCollision = this.checkCollision.bind(this)
+
     this.catLeft = "200px"
     this.catWidth = "120px"
     this.barrierWidth = "8px"
@@ -49,9 +53,6 @@ cgprt.render = function () {
     this.navigatorEl = new Navigator(this.jumpUp, this.jumpDown).render()
     this.catGameEl.appendChild(this.navigatorEl)
 
-    /* this.barrierEl = new Barrier().render()
-    this.catGameEl.appendChild(this.barrierEl) */
-
     this.healthLevelEl = new HealthLevel(this.levelCount).render()
     this.catGameEl.appendChild(this.healthLevelEl)
 
@@ -62,28 +63,62 @@ cgprt.render = function () {
 
 
 // из метода goGame мы будем управлять течением игры
-/* cgprt.goGame = function () {
-
-    // попробовать таймер через setTimeout
-    // он будет работать через равные промежутки времени?!
-    setTimeout(this.goGame, 5000);
-
-} */
-
 cgprt.goGame = function () {
-    this.barrierEl = new Barrier(this.topCoord, this.middleCoord, this.bottomCoord).render()
-    this.catGameEl.appendChild(this.barrierEl)
+    // игра начинается= барьер двигается, только если у нас еще есть жизни
     if (this.levelCount > 0) {
-        setTimeout(this.barrierMove, 500)
+        //барьер будет с каждым экраном перерисовываться
+        this.barrierEl = new Barrier(this.topCoord, this.middleCoord, this.bottomCoord).render()
+        this.catGameEl.appendChild(this.barrierEl)
 
+        //выбираем рандомное расположение барьера
+        this.barrierSelectLevel()
+        //прибавляем 500 ms, чтобы успеть экрану обновиться
+        setTimeout(this.barrierMove, 500)
     }
+    //зацикливаем вызов игры
+    //тоже потом очистим, когда красиво остановим игру
     setTimeout(this.goGame, 4600)
 
 }
 
+cgprt.barrierSelectLevel = function () {
+
+    //решаем какой уровень займет барьер
+
+    let randomLevel = Math.floor(Math.random() * 3)
+    switch (randomLevel) {
+        case 0:
+            this.barrierEl.classList.add("barrierOne")
+            // придумаем св-во типа линия движения, в него будем отдавать верхнюю, среднюю, нижнюю линию
+            // для большого количества препятствий, вероятно, проще будет заочно расчертить на невидимые промежутки-лиинии
+            this.barrierEl.lineOfMotion = -2
+            break
+
+        case 1:
+            this.barrierEl.classList.add("barrierTwo")
+            this.barrierEl.lineOfMotion = 0
+            break
+
+        case 2:
+            this.barrierEl.classList.add("barrierThree")
+            this.barrierEl.lineOfMotion = 2
+    }
+
+
+}
+
 cgprt.barrierMove = function () {
+    // добавляем класс с анимацией
     this.barrierEl.classList.add("barrierMove")
+    // говорим насколько сдвинуться барьеру
+    // это тоже как-то перенестив рандомный решатель
     this.barrierEl.style.transform = `translateX(${-610}px)`
+
+    // игра началась, запустили таймер на проверку столкновения через 1.5 секунды
+    // он будет работать примерно одну секунду(может и поменьше)
+    setTimeout(this.smash, 1500)
+
+    // по окончании одного экрана удаляем элемент
     setTimeout(this.barrierDelete, 4000)
 }
 
@@ -91,6 +126,41 @@ cgprt.barrierDelete = function () {
     this.catGameEl.removeChild(this.barrierEl)
 }
 
+cgprt.stopCheckCollison = function () {
+    clearInterval(this.timerCollision)
+}
+
+
+
+cgprt.smash = function () {
+    // зацикливаем таймер проверки на столкновение
+    this.timerCollision = setTimeout(this.smash, 250)
+
+
+
+    if (this.curentLevel == this.barrierEl.lineOfMotion) {
+        console.log('бум')
+        this.deleteHealthPoint()
+
+        // если столкновение произошло,  очистим таймер ожидания столкновения сразу
+        this.stopCheckCollison()
+        /* this.barrierEl.classList.add("barrierMoveDalay")
+        this.barrierEl.style.transform = `translateX(${-610}px)` */
+        // этот таймер нужно очистить
+    }
+    //если столкновение не произошло, то мы очистим таймер через 600ms
+    else (setTimeout(this.stopCheckCollison, 500))
+
+
+}
+cgprt.deleteHealthPoint = function () {
+    this.levelCount -= 1
+    this.catGameEl.removeChild(this.healthLevelEl)
+    this.healthLevelEl = new HealthLevel(this.levelCount).render()
+    this.catGameEl.appendChild(this.healthLevelEl)
+
+
+}
 
 
 
@@ -130,40 +200,7 @@ cgprt.jumpDown = function () {
 // проверяет столкновение с котом на период прохождения препятствия под котиком
 // приходится играть со временем, чтобы добиться оптимального эффекта
 // нужно ли потом очистить таймер setTimeout?!
-cgprt.checkCollision = function () {
-    //может быть id таймер вынести в конструктор
-    //после столкновения нам его тоже можно сразу очистить?!
 
-
-    this.checkCollisionTimerID = setInterval(this.smash, 100)
-    setTimeout(stopCollisionTimerID, 700)
-    function stopCollisionTimerID() {
-        clearInterval(this.checkCollisionTimerID), 1000
-    }
-
-    /*  setTimeout (clearInterval(checkCollisionTimerID), 1000) */
-
-
-}
-
-// не уверенна, что нужно было выносить в отдельный метод...
-
-cgprt.smash = function () {
-
-    if (this.curentLevel == 0) {
-        console.log('бум')
-        this.deleteHealthPoint()
-    }
-
-}
-cgprt.deleteHealthPoint = function () {
-    this.levelCount -= 1
-    this.catGameEl.removeChild(this.healthLevelEl)
-    this.healthLevelEl = new HealthLevel(this.levelCount).render()
-    this.catGameEl.appendChild(this.healthLevelEl)
-
-
-}
 
 
 
